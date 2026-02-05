f4b4acd6eed051b8d8ee2557006f809a4c8ee4a9
Nunna Charan Naga Lakshmi Narayana <109160426+nunnacharan@users.noreply.github.com>
2026-02-05 21:59:41 +0530

    Fix sendMail import path in server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const sendMail = require("./utils/sendMail");
+const sendMail = require("./utils/sendEmail");
 
 dotenv.config();
 const dns = require('dns');
@@ -77,4 +77,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`🚀 Server running on port ${PORT}`)
 );
+
 
