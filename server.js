0520526ccdc6cc56f010801443bb2721f8559bda
Nunna Charan Naga Lakshmi Narayana <109160426+nunnacharan@users.noreply.github.com>
2026-02-05 21:54:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const sendMail = require("./utils/sendMail");
 
 dotenv.config();
 const dns = require('dns');
@@ -57,7 +58,15 @@ app.use("/files", require("./routes/fileRoutes"));
 app.get("/", (req, res) => {
   res.send("🚀 API running successfully");
 });
+app.get("/mail-test", async (req, res) => {
+  await sendMail(
+    "yourpersonalemail@gmail.com",
+    "Test Mail 🚀",
+    "Hello Charan! Email working from Render 🎉"
+  );
 
+  res.send("Mail sent (check inbox)");
+});
 
 /* =================================================
    SERVER START
@@ -68,3 +77,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`🚀 Server running on port ${PORT}`)
 );
+
